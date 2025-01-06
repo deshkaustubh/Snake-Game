14a1ddd5943c05a99cf672b13ec9a08d98b7b521
Kaustubh <ksd.tcet@gmail.com>
2025-01-06 14:39:28 +0530

    Initial Push

---
+++ app/src/main/java/com/example/snakegame/SnakeGameScreen.kt
@@ -0,0 +1,78 @@
+package com.example.snakegame
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.unit.dp
+import com.example.snakegame.ui.theme.Custard
+import com.example.snakegame.ui.theme.RoyalBlue
+
+// snake Game Screen contains all the ui related code
+
+@Composable
+fun SnakeGameScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Card(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = "Score: 5",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(ratio = 2 /3f)
+        ){
+            val cellSize = size.width / 20
+            drawGameBoard(
+                cellSize = cellSize,
+                cellColor = Custard,
+                borderCellColor = RoyalBlue,
+                gridHeight = 30,
+                gridWidth = 20
+            )
+        }
+    }
+}
+
+private fun DrawScope.drawGameBoard(
+    cellSize : Float,
+    cellColor: Color,
+    borderCellColor: Color,
+    gridWidth: Int,
+    gridHeight: Int
+    ){
+    for (i in 0 until gridWidth) {
+        for (j in 0 until gridHeight) {
+            val isBorderCell = i == 0 || i == gridWidth - 1 || j == gridHeight - 1
+            drawRect(
+                color = if (isBorderCell) borderCellColor
+                        else if ((i + j ) % 2 == 0) cellColor
+                        else cellColor.copy(alpha = 0.5f),
+                topLeft = Offset(x = i * cellSize, y = j * cellSize),
+                size = Size(cellSize, cellSize)
+            )
+        }
+    }
+}
+
+// start from 9: 14
